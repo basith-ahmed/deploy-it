46a610941f3d3658cb9c5385751ebb22178d1e7f
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-05-11 23:46:14 +0530

    chore: initial builds

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/repo-url", (req, res) => {
+  const repoUrl = req.body.repoUrl;
+  console.log("Received repo URL:", repoUrl);
+  res.json({});
+});
+
+app.listen(3000);
